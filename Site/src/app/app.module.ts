222b26d09004d077b28df41bffe8707d6719e92b
FaresAbuIram <aburamf@gmail.com>
2020-12-03 12:33:17 +0200

    login

--- Site/src/app/app.module.ts
+++ Site/src/app/app.module.ts
@@ -8,14 +8,11 @@ import { BasiccontentComponent } from './pages/basiccontent/basiccontent.compone
 import { RegisterComponent } from './pages/register/register.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { OrdersComponent } from './pages/orders/orders.component';
-<<<<<<< HEAD
 import { LoginComponent } from './pages/login/login.component';
-=======
 import { MenuComponent } from './pages/menus/menu/menu.component';
 import { AddEditMenuComponent } from './pages/menus/add-edit-menu/add-edit-menu.component';
 import { HttpClientModule } from '@angular/common/http';
 
->>>>>>> 7608c833214ca529537d94b70a7498ea48f98b6c
 
 @NgModule({
   declarations: [
@@ -24,12 +21,9 @@ import { HttpClientModule } from '@angular/common/http';
     BasiccontentComponent,
     RegisterComponent,
     OrdersComponent,
-<<<<<<< HEAD
-    LoginComponent
-=======
+    LoginComponent,
     MenuComponent,
     AddEditMenuComponent
->>>>>>> 7608c833214ca529537d94b70a7498ea48f98b6c
   ],
   imports: [
     BrowserModule,
